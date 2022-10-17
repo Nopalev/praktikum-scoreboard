1ae76939e50dffb47e66485b5a61bf8841547cec
Nopalev <faadhilah56@gmail.com>
2022-10-17 12:06:53 +0700

    improve stylesheet

--- scripts/pagebuilder.js
+++ scripts/pagebuilder.js
@@ -9,9 +9,9 @@ function AddTitleAndHeader(){
     const pageTitle =   arrayOfString[4][0].toUpperCase() + 
                         arrayOfString[4].substring(1) + 
                         " Modul " + 
-                        arrayOfString[3] + 
+                        arrayOfString[2] + 
                         " Dasar Pemrograman " + 
-                        arrayOfString[4] + 
+                        arrayOfString[3] + 
                         " 2022/2023";
     document.title = pageTitle;
 
@@ -21,7 +21,7 @@ function AddTitleAndHeader(){
     document.getElementById("header").appendChild(headerText);
 }
 
-async function TableLoaderAndLegendCreator(){
+async function TableLoader(){
     const response1 = await fetch(scoreboardDirectory);
     const scoreboardData = await response1.json();
     const response2 = await fetch(teamsDirectory);
@@ -29,6 +29,7 @@ async function TableLoaderAndLegendCreator(){
     const table = document.createElement("table");
     const header = ["Rank", "Team", "Score", "A", "B", "C", "D"];
 
+    const thead = document.createElement("thead");
     const tr = document.createElement("tr"); 
 
     header.forEach(element => { // table header
@@ -38,7 +39,10 @@ async function TableLoaderAndLegendCreator(){
         tr.appendChild(th);
     });
 
-    table.appendChild(tr);    
+    thead.appendChild(tr);
+    table.appendChild(thead);
+    
+    const tbody = document.createElement("tbody");
 
     scoreboardData.rows.forEach(element => {
         const tr = document.createElement("tr");
@@ -102,41 +106,14 @@ async function TableLoaderAndLegendCreator(){
             tr.appendChild(verdict);
         });
 
-        table.appendChild(tr);
+        tbody.appendChild(tr);
 
     });
+    table.appendChild(tbody);
     document.getElementById("tableContainer").appendChild(table);
-
-    const legendContainer = document.createElement("div");
-    legendContainer.classList.add("Center");
-    legendContainer.setAttribute("id", "legendContainer");
-
-    const legend = document.createElement("table");
-    var letter = 'A';
-    scoreboardData.rows[0].problems.forEach(element => { // problem tag
-        const tr = document.createElement("tr");
-        tr.classList.add("LegendText");
-        const problemLetterCells = document.createElement("td");
-        const problemLetter = document.createElement("p");
-        problemLetter.appendChild(document.createTextNode(letter));
-        problemLetterCells.appendChild(problemLetter);
-        tr.appendChild(problemLetterCells);
-
-        const problemTagCells = document.createElement("td");
-        const problemTag = document.createElement("p");
-        problemTag.appendChild(document.createTextNode(element.label));
-        problemTagCells.appendChild(problemTag);
-        tr.appendChild(problemTagCells);
-
-        legend.appendChild(tr);
-        letter = String.fromCharCode(letter.charCodeAt(0) + 1);;
-    });
-    legendContainer.appendChild(legend);
-
-    document.getElementById("tableContainer").insertAdjacentElement("afterend", legendContainer);
 }
 
 function Init(){
     AddTitleAndHeader();
-    TableLoaderAndLegendCreator();
+    TableLoader();
 }
